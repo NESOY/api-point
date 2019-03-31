5636b977b1e0fe229cf1a9ea31f475c1ae55526d
YoungJae Kwon <kyoje11@gmail.com>
2019-03-31 21:33:46 +0900

    Fix Testcase : PointTest

--- src/test/java/com/triple/point/domain/PointTest.java
+++ src/test/java/com/triple/point/domain/PointTest.java
@@ -8,10 +8,10 @@ public class PointTest {
 
 	@Test
 	public void getPoint_컨텐츠가_1자_이상인_경우_점수를_반환한다() {
-		Point validContentPoint = Point.builder().content("Nesoy").build();
+		Point validContentPoint = Point.builder().review(new Review("VALID-REVIEW", "Nesoy")).build();
 		assertEquals(1L, (long) validContentPoint.getPoint());
 
-		Point invalidContentPoint = Point.builder().content("").build();
+		Point invalidContentPoint = Point.builder().review(new Review("INVALID-REVIEW", "")).build();
 		assertEquals(0L, (long) invalidContentPoint.getPoint());
 	}
 }
